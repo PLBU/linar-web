d23d8890bcdd182fb4230737c74fb7c9d45d29b1
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-08-22 21:49:03 +0700

    Project Card Draft #1

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,13 +1,21 @@
 import { type Component } from 'solid-js';
 
-import styles from './ProjectCard.module.css';
-import Image from '../image/Image';
 import PbImageGroup from '../pb-image-group/PbImageGroup';
+import ChipGroup from '../chip-group/ChipGroup';
+
+import styles from './ProjectCard.module.css';
 
 const ProjectCard: Component<{ project: Project }> = (props) => {
     return (
         <div class={styles.container}>
             <PbImageGroup project={props.project} />
+            <div class={styles.summaryContainer}>
+                <div class={styles.titleContainer}>
+                    <h3>{props.project ? props.project.title : "Loading ..."}</h3>
+                </div>
+                <ChipGroup texts={props.project ? props.project.technologies : ['Teching up']} />
+                <div class={`p2 ${styles.description}`}>{props.project ? props.project.description : "Waiting description ..."}</div>
+            </div>
         </div>
     )
 }
