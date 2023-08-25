750bd90e32ec1cfbf7bbddb0c3805a7c186789a0
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-08-25 21:01:26 +0700

    Add arrows and projects iterator

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,8 +6,12 @@ import styles from './projects.module.css';
 import ProjectCard from '../../components/project-card/ProjectCard';
 import pb from '../../service';
 
+import arrow from '../../assets/icons/arrow.svg'
+
 const ProjectsPage: Component = () => {
     const [projects, setProjects] = createSignal<Project[]>([])
+    const [idx, setIdx] = createSignal<number>(0)
+
     const fetchProjectsRemotely = async () => {
         const records: Project[] = await pb.collection('projects').getFullList({
             sort: '-created',
@@ -23,8 +27,12 @@ const ProjectsPage: Component = () => {
     return (
         <div class={styles.container}>
             <Title textBefore='Some of my ' highlight='Projects' />
+            <div class={styles.buttons}>
+                {idx() > 0 ? <img class={styles.left} src={arrow} onClick={() => setIdx(idx() - 1)}/> : null}
+                {idx() < projects().length - 1 ? <img class={styles.right} src={arrow} onClick={() => setIdx(idx() + 1)}/> : null}
+            </div>
             <div class={styles.projectsContainer}>
-                <ProjectCard project={projects()[0]} />
+                <ProjectCard project={projects()[idx()]} />
             </div>
         </div>
     )
