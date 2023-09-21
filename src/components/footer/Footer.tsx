9065d6064c816b0b7a660a5e8c93ac731b96b5ad
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-09-21 17:21:51 +0700

    Change email

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,9 +8,9 @@ const Footer: Component = () => {
         <div class={styles.container}>
             <Title isLight={true} textBefore="Let's " highlight='Connect' />
             <div>
-                <a href={'https://mail.google.com/mail/?view=cm&fs=1&to=renaldi.linar@gmail.com'} target='_blank' class={`p2 ${styles.description}`}>G-Mail: renaldi.linar | </a>
+                <a href={'https://mail.google.com/mail/?view=cm&fs=1&to=renaldi.linar@gmail.com'} target='_blank' class={`p2 ${styles.description}`}>E-Mail: renaldi@linar.my.id | </a>
                 <a href={'https://www.instagram.com/renaldiarlin/'} target='_blank' class={`p2 ${styles.description}`}>IG: renaldiarlin | </a>
-                <a href={'https://www.linkedin.com/in/renaldi-arlin/'} target='_blank' class={`p2 ${styles.description}`}>Linked In: Renaldi Arlin</a>
+                <a href={'https://www.linkedin.com/in/renaldi-arlin/'} target='_blank' class={`p2 ${styles.description}`}>LinkedIn: Renaldi Arlin</a>
             </div>
             <div class={`chip ${styles.credit}`}>&copy Renaldi Arlin 2023 | Made with ❤️ using Solid JS</div>
         </div>
